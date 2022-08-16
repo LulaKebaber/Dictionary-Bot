e7205fb7a8f9f4748b4085b0a56dd8d7956834b4
Kebaber <danial-baizak@mail.ru>
2022-08-16 23:42:12 +0600

    added new functions

--- main.py
+++ main.py
@@ -3,52 +3,62 @@ import telebot
 import pandas as pd
 from telebot import types
 
-token="my token"
+token="5466738128:AAH0alQqffVY1NcpEHccT0NCuo1z0pRLDtk"
 bot=telebot.TeleBot(token)
 
 # vocabulary csv file
 df = pd.read_csv('vocab.csv')
+userinfo = pd.read_csv('userinfo.csv')
+
+# start block
+# -----------------
+@bot.message_handler(commands = ['start'])
+def start(message):
+    if not (userinfo.userid.eq(message.from_user.id)).any():
+        userinfo.loc[len(df.index)] = [message.from_user.id, 0, 0]
+        userinfo.to_csv('userinfo.csv', index=False)
+        bot.send_message(message.chat.id, 'You have successfully registered! Try to "/add" new word.')
+    else:
+        bot.send_message(message.chat.id, 'You already registered! Try to "/add" new word.')
+# -----------------
 
 # word adding block
 # -----------------
-isAsken = False
-
-@bot.message_handler(commands = ['add'])    
+@bot.message_handler(commands = ['add'])
 def askAddWord(message):
-    global isAsken
-    msg = bot.send_message(message.chat.id, 'Enter the word: ')
-    bot.register_next_step_handler(msg, askAddTrans)
-    df.loc[len(df.index)] = [1, 1, 1]
-    isAsken = True
+    if (userinfo.userid.eq(message.from_user.id)).any():
+        msg = bot.send_message(message.chat.id, 'Enter the word: ')
+        bot.register_next_step_handler(msg, askAddTrans)
+        df.loc[len(df.index)] = [1, 1, 1, message.from_user.id]
+    else:
+        bot.send_message(message.chat.id, 'First of all, you should register (/start)')
 def askAddTrans(message):
-    global isAsken
-    if isAsken:
-        msg = bot.send_message(message.chat.id, 'Enter the translation: ')
-        bot.register_next_step_handler(msg, askAddType)
-        df.word[len(df.index) - 1] = message.text       
+    msg = bot.send_message(message.chat.id, 'Enter the translation: ')
+    bot.register_next_step_handler(msg, askAddType)
+    df.word[len(df.index) - 1] = message.text       
 def askAddType(message):
-    global isAsken
-    if isAsken:
-        msg = bot.send_message(message.chat.id, 'Choose the type: noun/verb/adjective')
-        bot.register_next_step_handler(msg, saveAddedWord)
-        df.translation[len(df.index) - 1] = message.text
+    df.translation[len(df.index) - 1] = message.text
+    msg = bot.send_message(message.chat.id, 'Choose the type (e.g. noun/verb):')
+    bot.register_next_step_handler(msg, saveAddedWord)
 def saveAddedWord(message):
-    global isAsken
-    if isAsken:
-        df.type[len(df.index) - 1] = message.text
-        df.to_csv('vocab.csv', index=False)
-        isAsken = False
+    df.type[len(df.index) - 1] = message.text
+    userinfo['words count'][userinfo.userid == message.from_user.id] += 1
+    df.to_csv('vocab.csv', index=False)
+    userinfo.to_csv('userinfo.csv', index=False)
 # -----------------
 
 # show list of words
 # -----------------
 @bot.message_handler(commands = ['show'])
 def askTypeWords(message):
-    msg = bot.send_message(message.chat.id, 'Choose the the type: ')
-    bot.send_message(message.chat.id, 'Types: noun/adjective/verb')
-    bot.register_next_step_handler(msg, showList)
+    if (userinfo.userid.eq(message.from_user.id)).any():
+        msg = bot.send_message(message.chat.id, 'Choose the the type: ')
+        bot.send_message(message.chat.id, 'Your types: ' + str(list(df[df.userid == message.from_user.id].type.unique())))
+        bot.register_next_step_handler(msg, showList)
+    else:
+        bot.send_message(message.chat.id, 'First of all, you should register (/start)')
 def showList(message):
-    if message.text in ['noun', 'adjective', 'verb']:
+    if message.text in list(df[df.userid == message.from_user.id].type.unique()):
         words = list(df[df.type == message.text].word)
         translations = list(df[df.type == message.text].translation)
         for i in range(len(words)):
@@ -62,8 +72,11 @@ def showList(message):
 # -----------------
 @bot.message_handler(commands = ['remove'])
 def askDeleteWord(message):
-    msg = bot.send_message(message.chat.id, 'Write the word you want to delete: ')
-    bot.register_next_step_handler(msg, deleteWord)
+    if (userinfo.userid.eq(message.from_user.id)).any():
+        msg = bot.send_message(message.chat.id, 'Write the word you want to delete: ')
+        bot.register_next_step_handler(msg, deleteWord)
+    else:
+        bot.send_message(message.chat.id, 'First of all, you should register (/start)')
 def deleteWord(message):
     if (df.word.eq(message.text)).any():
         df.drop(df[df.word == message.text].index, inplace=True, axis=0)
@@ -73,17 +86,37 @@ def deleteWord(message):
         bot.send_message(message.chat.id, 'There is no such word in dictionary!')
 # -----------------
 
-# words learning
+# words checking
+# -----------------
 @bot.message_handler(commands = ['learn'])
 def askLearnType(message):
     msg = bot.send_message(message.chat.id, 'Choose the the type: ')
     bot.send_message(message.chat.id, 'Types: noun/adjective/verb')
     bot.register_next_step_handler(msg, learnWords)   
 def learnWords(message):
-      pass
-def on():
+    msg = bot.send_message(message.chat.id, df.word.sample(n=1))
+    bot.send_message(message.chat.id, 'Enter the translation: ')
+    bot.register_next_step_handler(msg, checkAnswer)
+def checkAnswer(message):
     pass
+# -----------------
 
+
+
+
+
+
+
+# # type adding
+# # -----------------
+# @bot.message_handler(commands = ['addtype'])
+# def askAddNewType(message):
+#     msg = bot.send_message(message.chat.id, 'Write the type you want to add: ')
+#     bot.register_next_step_handler(msg, addNewType)
+# def addNewType(message):
+#     if not (df.type.eq(message.text)).any():
+#         df.loc[len(df.index)] = [1, 1, message.text, message.from_user.id]
+#         bot.send_message(message.chat.id, 'Congratulations! You have added new type!')
 
 # ------------------
 # @bot.message_handler(commands = ['addword'])
